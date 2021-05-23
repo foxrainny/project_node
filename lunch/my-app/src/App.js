18b6dc345211a3dae2b680a4edf8beed9c9b941b
root <chhqm2759@naver.com>
2021-05-23 03:57:53 +0000

    front_work_1 react- gis code add !

---
+++ lunch/my-app/src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import Location from '/components/main.js';
+
+const App = () => {
+  return {Location};
+}
+
+export default App;
---
+++ lunch/my-app/src/components/main.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const Location = () =>
+{
+const [lat, setLat] = useState(null);
+const [lng, setLng] = useState(null);
+const [status, setStatus] = useState(null);
+
+const getLocation = () => {
+    if (!navigator.geolocation) {
+        setStatus('Geolocation is not supported by your browser');
+    } else {
+        setStatus('Locating...');
+        navigator.geolocation.getCurrentPosition((position) => {
+        setStatus(null);
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        fetch('주소', {
+            method: 'post',
+            body: JSON.stringify({
+            latitude: position.coords.latitude,
+            longtitude: position.coords.longtitude
+            })
+        })
+        }, () => {
+        setStatus('Unable to retrieve your location');
+        });
+    }
+}
+
+    return (
+    <div className="App">
+        <button onClick={getLocation}>Get Location</button>
+        <h1>현재 위치를 알려주세요 !(버튼 클릭)</h1>
+        <p>{status}</p>
+        {lat && <p>Latitude: {lat}</p>}
+        {lng && <p>Longitude: {lng}</p>}
+    </div>
+    );
+}
+  export default Location;
