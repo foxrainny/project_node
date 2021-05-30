be081f63c5164fba8f8b8eeb2d865e0dd982a155
root <chhqm2759@naver.com>
2021-05-30 03:19:38 +0000

    front_work_2_add_setProxy.js

---
+++ lunch/my-app/src/setProxy.js
@@ -0,0 +1,9 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        proxy('/api_v1', {
+            target: 'http://localhost:3001'
+        })
+    )
+}
