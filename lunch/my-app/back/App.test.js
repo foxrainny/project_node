fc36cedbb08cb4298a4f8bc9c0ab43e2baf00a85
foxrainny <lllccc111@naver.com>
2021-05-30 03:56:55 +0000

    work_4_react-nodejs 연동2 server response_test
    서버 respose 받아서 출력

--- lunch/my-app/back/App.test.js
+++ lunch/my-app/back/App.test.js
@@ -3,8 +3,11 @@ const express = require('express') , http = require('http'), path = require('pat
 var app = express()
 const port = 3001
 
-app.get('/api_v1',function(req,res){                                                                                  
-    res.send('api_v1');
+app.use(express.json({limit: '50mb'}));
+//app.use(express.json.urlencode({limit: '50mb', extened: true}));
+
+app.post('/api_v1',function(req,res){                                                                                  
+    res.send(req.body);
 });
 
 app.listen(port, function() {
